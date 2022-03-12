c50e67346243dc599f7a56e9a184c21b7ddfab70
Shaik-Amreen <69184237+Shaik-Amreen@users.noreply.github.com>
2022-03-12 13:01:22 +0530

    sih1

--- src/Farmer.js
+++ src/Farmer.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, Image, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, Image, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import axios from 'axios';
-
+import Automatedvoice from './Automatedvoice';
 import Voice from './Voice';
-
 
 
 
@@ -14,13 +13,7 @@ function HomeScreen() {
 
 
     const [farmdata, setfarmdata] = useState([
-        {
-            discount: { quantity: '10kg', price: '200' },
-            image: require("../assets/plus.gif"),
-            price: { vegtype: 'You can click here to proceed with separate category', price: '100' },
-            typeid: "12",
-            _id: "62278e73e3197fd5361e388e"
-        },
+
         {
             discount: { quantity: '10kg', price: '200' },
             image: require("../assets/Carrot.gif"),
@@ -28,7 +21,6 @@ function HomeScreen() {
             typeid: "12",
             _id: "62278e73e3197fd5361e388d"
         },
-
         {
             discount: { quantity: '10kg', price: '200' },
             image: require("../assets/Tomato.gif"),
@@ -120,13 +112,49 @@ function HomeScreen() {
             price: { vegtype: 'Radish', price: '100' },
             typeid: "12",
             _id: "62278e73e3197fd5361e388e"
-        }
+        },
+        {
+            discount: { quantity: '10kg', price: '200' },
+            image: require("../assets/plus.gif"),
+            price: { vegtype: ' separate category', price: '100' },
+            typeid: "12",
+            _id: "62278e73e3197fd5361e388e"
+        },
     ])
+
+    const [bill, setbill] = useState()
+
+
+    const [vis, setvis] = useState(false)
+    const [typename, settypename] = useState('')
+    const [congo, setcongo] = useState('')
+    const [perkg, setperkg] = useState('')
+    const [diskg, setdiskg] = useState('')
+    const [disprice, setdisprice] = useState('')
+    const [avail, setavail] = useState('')
+    const presscontent = (e) => {
+        setvis(true)
+        settypename(e)
+        setcongo('')
+    }
+
+    const pressbutton = () => {
+        setvis(false)
+    }
 
 
     useEffect(() => {
         fetchdata()
     }, [])
+
+
+    const pressok = () => {
+        setvis(false)
+        setcongo('done')
+        setbill([...diskg, ...disprice, ...perkg])
+        console.log(bill, "hyyyyyy")
+    }
+
     const fetchdata = () => {
         axios.post(`http://localhost:4000/farmer/finddetails`).then((response) => {
             // setfarmdata(response.data)
@@ -140,20 +168,72 @@ function HomeScreen() {
         <ScrollView >
             <ScrollView>
                 {farmdata.map((e) =>
-                    <View key={e._id} style={{ elevation: 5, backgroundColor: 'white', margin: 30 }}>
+                    <View key={e.price.vegtype} style={{ elevation: 5, backgroundColor: 'white', margin: 30 }}>
                         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                            <Voice {...{ "name": `Hey buddy , This is ${e.price.vegtype} , Click on it , if you want to proceed to sell your product , in this category .` }} />
-                            <Image
-                                source={e.image}
-                                style={{ width: 320, height: 320 }}
-                            />
+                            <Voice {...{ "name": `Hello , This is ${e.price.vegtype} , Click on it , if you want to proceed to sell your product , in this category .` }} />
+
+                            <TouchableOpacity onPress={() => presscontent(e.price.vegtype)}>
+
+                                <Image
+
+                                    source={e.image}
+                                    style={{ width: 320, height: 320 }}
+                                />
+                            </TouchableOpacity>
                         </View>
 
                         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginBottom: 30 }}>
                             <Text style={styles.heading}>{e.price.vegtype.toLocaleUpperCase()}</Text>
                             {/* <Text style={{ color: 'grey' }}>{e.price.price} ruppes per kg</Text>
                             <Text style={{ color: 'grey' }}>  {e.discount.price} rupees only more than {e.discount.quantity} kg</Text> */}
+                            {vis && typename == e.price.vegtype ?
+                                <View>
+
+                                    <Automatedvoice name="Hey ! enter price per kg and discount information in the below fields to proceed further " />
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder="enter price per kg"
+                                        keyboardType="numeric"
+                                        onChangeText={setperkg}
+
+                                    />
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder="enter discount range"
+                                        keyboardType="numeric"
+                                        onChangeText={setdiskg}
+                                    />
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setdisprice}
+
+
+                                        placeholder="enter  discount price "
+                                        keyboardType="numeric"
+                                    />
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setavail}
+                                        placeholder="enter  availability "
+                                        keyboardType="numeric"
+                                    />
+                                    {(congo == 'done') ? <Automatedvoice name="Hey ! enter price per kg and discount information in the below fields to proceed further " /> : null}
+                                    <TouchableOpacity
+                                        style={styles.button} >
+                                        <Text style={styles.text} onPress={pressok}>SUBMIT</Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        style={styles.buttondanger} onPress={pressbutton}>
+                                        <Text style={styles.text}>CLOSE</Text>
+                                    </TouchableOpacity>
+
+
+
+                                </View>
+
+                                : null}
                         </View>
+
                     </View>)}
             </ScrollView>
         </ScrollView>
@@ -162,11 +242,32 @@ function HomeScreen() {
 
 function SettingsScreen() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings!</Text>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', margin: 60 }}>
+            <Text>Congratulations ! </Text>
+            <Text>category : Tomato</Text>
+            <Text>Price per kg : 40</Text>
+            <Text>Disount kg : 10</Text>
+            <Text>Disount price : 30</Text>
         </View>
     );
 }
+
+function Fertilizers() {
+
+    return (
+        <View style={styles.container}>
+            <Voice {...{ "name": `Hello , This is compost , Click on it , to pruchase` }} />
+            <Image
+
+                source={require('../assets/fertlizer.jpg')}
+                style={{ width: 320, height: 320 }}
+            />
+
+        </View>
+    )
+
+}
+
 
 const Tab = createBottomTabNavigator();
 
@@ -182,7 +283,7 @@ export default function Farmer() {
                             iconName = focused
                                 ? 'ios-information-circle'
                                 : 'ios-information-circle-outline';
-                        } else if (route.name === 'Settings') {
+                        } else if (route.name === 'History') {
                             iconName = focused ? 'ios-list' : 'ios-list';
                         }
 
@@ -194,7 +295,9 @@ export default function Farmer() {
                 })}
             >
                 <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
+                <Tab.Screen name="History" component={SettingsScreen} />
+                <Tab.Screen name="Fertilizersname" component={Fertilizers} />
+
             </Tab.Navigator>
         </NavigationContainer>
     );
@@ -205,11 +308,34 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         width: '100%',
-        backgroundColor: 'red'
     },
     heading: {
         color: '#1b1c1e',
         fontWeight: '500',
         fontSize: 20
+    }, input: {
+        height: 40,
+        margin: 5,
+        borderWidth: 1,
+        padding: 10,
+        width: '100%',
+        marginBottom: 30,
+        borderColor: '#d3d3d3'
+    },
+    button: {
+        alignItems: "center",
+        backgroundColor: "green",
+        padding: 10,
+        marginBottom: 20,
+
+
+    },
+    buttondanger: {
+        alignItems: "center",
+        backgroundColor: "crimson",
+        padding: 10,
+
+    }, text: {
+        color: 'white'
     }
 });
